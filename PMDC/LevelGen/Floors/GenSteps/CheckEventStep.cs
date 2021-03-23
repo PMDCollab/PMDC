1eb897f25fc295710ed99b56bbd444ba697908e7
audino <2676737+audinowho@users.noreply.github.com>
2021-03-22 16:34:21 -0800

    Add other event steps

--- PMDC/LevelGen/Floors/GenSteps/CheckEventStep.cs
+++ PMDC/LevelGen/Floors/GenSteps/CheckEventStep.cs
@@ -9,6 +9,40 @@ using PMDC.Dungeon;
 namespace PMDC.LevelGen
 {
     [Serializable]
+    public class PrepareEventStep<T> : GenStep<T> where T : BaseMapGenContext
+    {
+        public List<SingleCharEvent> Events;
+        public PrepareEventStep() { Events = new List<SingleCharEvent>(); }
+        public PrepareEventStep(params SingleCharEvent[] events) : this()
+        {
+            Events.AddRange(events);
+        }
+
+        public override void Apply(T map)
+        {
+            foreach(SingleCharEvent check in Events)
+                map.PrepareEvents.Add((SingleCharEvent)check.Clone());
+        }
+    }
+
+    [Serializable]
+    public class StartEventStep<T> : GenStep<T> where T : BaseMapGenContext
+    {
+        public List<SingleCharEvent> Events;
+        public StartEventStep() { Events = new List<SingleCharEvent>(); }
+        public StartEventStep(params SingleCharEvent[] events) : this()
+        {
+            Events.AddRange(events);
+        }
+
+        public override void Apply(T map)
+        {
+            foreach (SingleCharEvent check in Events)
+                map.StartEvents.Add((SingleCharEvent)check.Clone());
+        }
+    }
+
+    [Serializable]
     public class CheckEventStep<T> : GenStep<T> where T : BaseMapGenContext
     {
         public List<SingleCharEvent> Events;
@@ -20,9 +54,8 @@ namespace PMDC.LevelGen
 
         public override void Apply(T map)
         {
-            foreach(SingleCharEvent check in Events)
+            foreach (SingleCharEvent check in Events)
                 map.CheckEvents.Add((SingleCharEvent)check.Clone());
         }
     }
-
 }
