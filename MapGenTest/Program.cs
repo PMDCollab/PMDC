8b1dce6a8a27b0127e7103929b04113c7d07e242
audino <2676737+audinowho@users.noreply.github.com>
2021-04-23 12:58:46 -0800

    fallbacks and update to mapgentest

--- MapGenTest/Program.cs
+++ MapGenTest/Program.cs
@@ -3,21 +3,41 @@ using RogueElements;
 using RogueEssence;
 using RogueEssence.Data;
 using RogueEssence.Content;
+using RogueEssence.Script;
 
 namespace MapGenTest
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             Console.WindowWidth = Console.LargestWindowWidth;
             Console.WindowHeight = Console.LargestWindowHeight;
             //Console.OutputEncoding = Encoding.UTF8;
 
+            string[] args = Environment.GetCommandLineArgs();
+            PathMod.InitExePath(System.IO.Path.GetDirectoryName(args[0]));
             DiagManager.InitInstance();
+
+
+            for (int ii = 1; ii < args.Length; ii++)
+            {
+                if (args[ii] == "-asset")
+                {
+                    PathMod.ASSET_PATH = System.IO.Path.GetFullPath(PathMod.ExePath + args[ii + 1]);
+                    ii++;
+                }
+                else if (args[ii] == "-raw")
+                {
+                    PathMod.DEV_PATH = System.IO.Path.GetFullPath(PathMod.ExePath + args[ii + 1]);
+                    ii++;
+                }
+            }
+
             GraphicsManager.InitParams();
             Text.Init();
             Text.SetCultureCode("en");
+            LuaEngine.InitInstance();
             DataManager.InitInstance();
             DataManager.Instance.InitData();
 #if DEBUG
