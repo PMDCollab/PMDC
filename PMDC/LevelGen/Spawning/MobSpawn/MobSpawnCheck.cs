f2352ebe6275aeab8362af9efda5daf035cc8607
audino <2676737+audinowho@users.noreply.github.com>
2021-09-21 22:29:04 -0800

    MobSpawnConditions and tunnel step fix

---
+++ PMDC/LevelGen/Spawning/MobSpawn/MobSpawnCheck.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using RogueEssence.Dungeon;
+using RogueEssence.Data;
+using RogueElements;
+using RogueEssence.LevelGen;
+using PMDC.Data;
+using System.Collections.Generic;
+using RogueEssence.Dev;
+using RogueEssence.Script;
+using NLua;
+using System.Linq;
+
+namespace PMDC.LevelGen
+{
+    /// <summary>
+    /// Spawns the mob if it matches version ID.
+    /// </summary>
+    [Serializable]
+    public class MobCheckVersionDiff : MobSpawnCheck
+    {
+        public int Remainder;
+        public int Div;
+
+        public MobCheckVersionDiff()
+        {
+
+        }
+        public MobCheckVersionDiff(int remainder, int div)
+        {
+            Div = div;
+            Remainder = remainder;
+        }
+        public MobCheckVersionDiff(MobCheckVersionDiff other) : this()
+        {
+            Remainder = other.Remainder;
+            Div = other.Div;
+        }
+        public override MobSpawnCheck Copy() { return new MobCheckVersionDiff(this); }
+
+        public override bool CanSpawn()
+        {
+            return DataManager.Instance.Save.Rand.FirstSeed % (ulong)Div == (ulong)Remainder;
+        }
+
+    }
+
+}
