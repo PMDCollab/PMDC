b0f1ed4db70ff8f11ae2737cd824ecb3b0413be7
audino <2676737+audinowho@users.noreply.github.com>
2021-12-03 14:17:38 -0800

    Move assembly version into csproj

--- PMDC/Properties/AssemblyInfo.cs
+++ PMDC/Properties/AssemblyInfo.cs
@@ -20,15 +20,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("898ca2a9-57be-4bbc-9fe7-fae5d6818124")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.5.2.1")]
