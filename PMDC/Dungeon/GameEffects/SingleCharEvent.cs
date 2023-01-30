1f2291288b8423dd8c58482444e9bc5a45f8a230
audino <2676737+audinowho@users.noreply.github.com>
2023-01-29 17:26:02 -0800

    abstract out title dropping

--- PMDC/Dungeon/GameEffects/SingleCharEvent.cs
+++ PMDC/Dungeon/GameEffects/SingleCharEvent.cs
@@ -158,6 +158,11 @@ namespace PMDC.Dungeon
 
         public override List<Character> RespawnMob()
         {
+            return Respawn();
+        }
+
+        public static List<Character> Respawn()
+        {
             List<Character> respawns = new List<Character>();
             if (ZoneManager.Instance.CurrentMap.TeamSpawns.CanPick)
             {
@@ -3836,6 +3841,27 @@ namespace PMDC.Dungeon
                     character.CharLoc = ZoneManager.Instance.CurrentMap.EntryPoints[0].Loc;
             }
             character.CharDir = ZoneManager.Instance.CurrentMap.EntryPoints[0].Dir;
+        }
+    }
+
+    [Serializable]
+    public class FadeTitleEvent : SingleCharEvent
+    {
+        public FadeTitleEvent() { }
+        public override GameEvent Clone() { return new FadeTitleEvent(); }
+
+        public override IEnumerator<YieldInstruction> Apply(GameEventOwner owner, Character ownerChar, SingleCharContext context)
+        {
+            if (context.User != null)
+                yield break;
+
+            // title drop if faded, but do not fade directly
+            if (GameManager.Instance.IsFaded())
+            {
+                yield return CoroutineManager.Instance.StartCoroutine(GameManager.Instance.FadeTitle(true, ZoneManager.Instance.CurrentMap.Name.ToLocal()));
+                yield return new WaitForFrames(30);
+                yield return CoroutineManager.Instance.StartCoroutine(GameManager.Instance.FadeTitle(false, ""));
+            }
         }
     }
 
---
+++ PMDC/LevelGen/Zones/ZoneSteps/FloorNameDropZoneStep.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using RogueElements;
+using RogueEssence.LevelGen;
+using PMDC.Dungeon;
+using RogueEssence;
+using System.Runtime.Serialization;
+
+namespace PMDC.LevelGen
+{
+    /// <summary>
+    /// Names all floors of the dungeon segment according to a naming convention.
+    /// </summary>
+    [Serializable]
+    public class FloorNameDropZoneStep : FloorNameIDZoneStep
+    {
+        /// <summary>
+        /// At what point in the map start to drop the title.
+        /// </summary>
+        public Priority DropPriority;
+
+        public FloorNameDropZoneStep()
+        {
+        }
+
+        public FloorNameDropZoneStep(Priority priority, LocalText name, Priority dropPriority) : base(priority, name)
+        {
+            DropPriority = dropPriority;
+        }
+        protected FloorNameDropZoneStep(FloorNameDropZoneStep other, ulong seed) : base(other, seed)
+        {
+            DropPriority = other.DropPriority;
+        }
+
+        public override ZoneStep Instantiate(ulong seed) { return new FloorNameDropZoneStep(this, seed); }
+
+        public override void Apply(ZoneGenContext zoneContext, IGenContext context, StablePriorityQueue<Priority, IGenStep> queue)
+        {
+            base.Apply(zoneContext, context, queue);
+
+            MapEffectStep<BaseMapGenContext> fade = new MapEffectStep<BaseMapGenContext>();
+            fade.Effect.OnMapStarts.Add(DropPriority, new FadeTitleEvent());
+            queue.Enqueue(Priority, fade);
+        }
+
+
+        [OnDeserialized]
+        internal void OnDeserializedMethod(StreamingContext context)
+        {
+            //TODO: Remove in v1.1
+            if (RogueEssence.Data.Serializer.OldVersion < new Version(0, 7, 0))
+                DropPriority = new Priority(-15);
+        }
+    }
+}
