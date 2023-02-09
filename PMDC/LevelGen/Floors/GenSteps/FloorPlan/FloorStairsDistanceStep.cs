b46cceff0e5b60e9debe759c5654e451dedeb4a0
audino <2676737+audinowho@users.noreply.github.com>
2023-02-08 22:58:53 -0800

    spacing for spawns

---
+++ PMDC/LevelGen/Floors/GenSteps/FloorPlan/FloorStairsDistanceStep.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using RogueElements;
+
+namespace PMDC.LevelGen
+{
+    /// <summary>
+    /// Adds the entrance and exit to the floor.  Is room-conscious.
+    /// The algorithm will try to place them within and outside of a certain specified range.
+    /// </summary>
+    /// <typeparam name="TGenContext"></typeparam>
+    /// <typeparam name="TEntrance"></typeparam>
+    /// <typeparam name="TExit"></typeparam>
+    [Serializable]
+    public class FloorStairsDistanceStep<TGenContext, TEntrance, TExit> : FloorStairsStep<TGenContext, TEntrance, TExit>
+        where TGenContext : class, IFloorPlanGenContext, IPlaceableGenContext<TEntrance>, IPlaceableGenContext<TExit>
+        where TEntrance : IEntrance
+        where TExit : IExit
+    {
+        public FloorStairsDistanceStep()
+        {
+        }
+
+        public FloorStairsDistanceStep(IntRange range, TEntrance entrance, TExit exit) : base(entrance, exit)
+        {
+            Distance = range;
+        }
+
+        public FloorStairsDistanceStep(IntRange range, List<TEntrance> entrances, List<TExit> exits) : base(entrances, exits)
+        {
+            Distance = range;
+        }
+
+        /// <summary>
+        /// Range of distance in tiles that entrances and exits mut be apart.  start-inclusive, end-exclusive
+        /// </summary>
+        public IntRange Distance { get; set; }
+
+        /// <summary>
+        /// Attempt to choose an outlet in a room with no entrance/exit, and updates their availability.  If none exists, default to a chosen room.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="map"></param>
+        /// <param name="free_indices"></param>
+        /// <param name="used_indices"></param>
+        /// <returns></returns>
+        protected override Loc? GetOutlet<T>(TGenContext map, List<int> free_indices, List<int> used_indices)
+        {
+            while (free_indices.Count > 0)
+            {
+                int roomIndex = map.Rand.Next() % free_indices.Count;
+                int startRoom = free_indices[roomIndex];
+
+                Rect startDraw = map.RoomPlan.GetRoom(startRoom).Draw;
+
+                bool used = false;
+                foreach (int usedRoom in used_indices)
+                {
+                    Rect usedDraw = map.RoomPlan.GetRoom(usedRoom).Draw;
+                    if (!Distance.Contains((usedDraw.Start - startDraw.Start).Dist4()))
+                    {
+                        used = true;
+                        break;
+                    }
+                }
+
+                if (used)
+                {
+                    // if we're not on our backup list, move it to the backup list and continue on
+                    if (used_indices != null)
+                    {
+                        free_indices.RemoveAt(roomIndex);
+                        //TODO: come up with a third list for indices that can still be used in the final backup plan
+                        //but must be avoided when checking existing used indices
+                        //used_indices.Add(startRoom);
+                        continue;
+                    }
+                }
+
+                List<Loc> tiles = ((IPlaceableGenContext<T>)map).GetFreeTiles(startDraw);
+
+                if (tiles.Count == 0)
+                {
+                    // this room is not suitable and never will be, remove it
+                    free_indices.RemoveAt(roomIndex);
+                    continue;
+                }
+
+                Loc start = tiles[map.Rand.Next(tiles.Count)];
+
+                // if we have a used-list, transfer the index over
+                if (used_indices != null)
+                {
+                    free_indices.RemoveAt(roomIndex);
+                    used_indices.Add(startRoom);
+                }
+
+                return start;
+            }
+
+            return null;
+        }
+    }
+}
---
+++ PMDC/LevelGen/Spawning/SpacedRoomSpawnStep.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using RogueElements;
+using RogueEssence.Dungeon;
+using PMDC.Dungeon;
+using RogueEssence.LevelGen;
+
+namespace PMDC.LevelGen
+{
+    /// <summary>
+    /// Spawns objects in randomly chosen rooms.  Once a room is chosen, it
+    /// Large rooms have the same probability as small rooms.
+    /// </summary>
+    /// <typeparam name="TGenContext"></typeparam>
+    /// <typeparam name="TSpawnable"></typeparam>
+    [Serializable]
+    public class SpacedRoomSpawnStep<TGenContext, TSpawnable> : RoomSpawnStep<TGenContext, TSpawnable>
+        where TGenContext : class, IFloorPlanGenContext, IPlaceableGenContext<TSpawnable>
+        where TSpawnable : ISpawnable
+    {
+        public SpacedRoomSpawnStep()
+            : base()
+        {
+        }
+
+        public SpacedRoomSpawnStep(IStepSpawner<TGenContext, TSpawnable> spawn, bool includeHalls = false)
+            : base(spawn)
+        {
+            this.IncludeHalls = includeHalls;
+        }
+
+        /// <summary>
+        /// Makes halls eligible for spawn.
+        /// </summary>
+        public bool IncludeHalls { get; set; }
+
+        public override void DistributeSpawns(TGenContext map, List<TSpawnable> spawns)
+        {
+            HashSet<RoomHallIndex> takenRooms = new HashSet<RoomHallIndex>();
+
+            // random per room, not per-tile
+            var spawningRooms = new SpawnList<RoomHallIndex>();
+            var remainingRooms = new SpawnList<RoomHallIndex>();
+
+            for (int ii = 0; ii < map.RoomPlan.RoomCount; ii++)
+            {
+                if (!BaseRoomFilter.PassesAllFilters(map.RoomPlan.GetRoomPlan(ii), this.Filters))
+                    continue;
+                spawningRooms.Add(new RoomHallIndex(ii, false), 10);
+            }
+
+            if (this.IncludeHalls)
+            {
+                for (int ii = 0; ii < map.RoomPlan.HallCount; ii++)
+                {
+                    if (!BaseRoomFilter.PassesAllFilters(map.RoomPlan.GetHallPlan(ii), this.Filters))
+                        continue;
+                    spawningRooms.Add(new RoomHallIndex(ii, true), 10);
+                }
+            }
+
+            while (spawningRooms.Count > 0 && spawns.Count > 0)
+            {
+                int randIndex = spawningRooms.PickIndex(map.Rand);
+                RoomHallIndex roomIndex = spawningRooms.GetSpawn(randIndex);
+
+                if (takenRooms.Contains(roomIndex))
+                {
+                    spawningRooms.RemoveAt(randIndex);
+                    remainingRooms.Add(roomIndex, 10);
+                    continue;
+                }
+
+                // try to spawn the item
+                if (this.SpawnInRoom(map, roomIndex, spawns[spawns.Count - 1]))
+                {
+                    GenContextDebug.DebugProgress("Placed Object");
+
+                    // remove the item spawn
+                    spawns.RemoveAt(spawns.Count - 1);
+
+                    spawningRooms.RemoveAt(randIndex);
+                    takenRooms.Add(roomIndex);
+
+                    //add adjacents to the takenRooms
+                    List<RoomHallIndex> adjacent = map.RoomPlan.GetRoomHall(roomIndex).Adjacents;
+                    for (int ii = 0; ii < adjacent.Count; ii++)
+                        takenRooms.Add(adjacent[ii]);
+
+                    if (!roomIndex.IsHall)
+                    {
+                        List<int> adjacentRooms = map.RoomPlan.GetAdjacentRooms(roomIndex.Index);
+                        for (int ii = 0; ii < adjacentRooms.Count; ii++)
+                            takenRooms.Add(new RoomHallIndex(ii, false));
+                    }
+                }
+                else
+                {
+                    spawningRooms.RemoveAt(randIndex);
+                }
+            }
+
+            //backup plan; spawn in remaining rooms
+            this.SpawnRandInCandRooms(map, spawningRooms, spawns, 100);
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}<{1}>: WithHalls:{2}", this.GetType().Name, typeof(TSpawnable).Name, this.IncludeHalls);
+        }
+    }
+}
