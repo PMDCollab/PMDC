6dd33d5ef03fd301b626af1b1f07a064ecf27b02
Touhou <122487411+plskbs@users.noreply.github.com>
2023-05-28 23:22:25 -0500

    Adds universal states for monster house settings

--- PMDC/Dungeon/GameEffects/UniversalState.cs
+++ PMDC/Dungeon/GameEffects/UniversalState.cs
@@ -227,4 +227,45 @@ namespace PMDC.Dungeon
         }
     }
 
+    [Serializable]
+    public class MonsterHouseTableState : UniversalState
+    {
+        /// <summary>
+        /// If this is set, this tile will be used to display a warning for monster houses.
+        /// </summary>
+        [DataType(0, DataManager.DataType.Tile, false)]
+        public string MonsterHouseWarningTile;
+        
+        /// <summary>
+        /// If this is set, this tile will be used to replace the chest in a chest ambush.
+        /// </summary>
+        [DataType(0, DataManager.DataType.Tile, false)]
+        public string ChestAmbushWarningTile;
+
+        /// <summary>
+        /// If this is set to true, you will not be able to spawn into a monster house upon entering a floor.
+        /// </summary>
+        public bool NoMonsterHouseEntrances;
+
+        public MonsterHouseTableState()
+        {
+            MonsterHouseWarningTile = null;
+            ChestAmbushWarningTile = null;
+            NoMonsterHouseEntrances = false;
+        }
+
+        public MonsterHouseTableState(string monsterHouseWarningTile, string chestAmbushWarningTile, bool noMonsterHouseEntrances)
+        {
+            MonsterHouseWarningTile = monsterHouseWarningTile; 
+            ChestAmbushWarningTile = chestAmbushWarningTile; 
+            NoMonsterHouseEntrances = noMonsterHouseEntrances;
+        }
+        protected MonsterHouseTableState(MonsterHouseTableState other)
+        {
+            MonsterHouseWarningTile = other.MonsterHouseWarningTile;
+            ChestAmbushWarningTile = other.ChestAmbushWarningTile;
+            NoMonsterHouseEntrances = other.NoMonsterHouseEntrances;
+        }
+        public override GameplayState Clone() { return new MonsterHouseTableState(this); }
+    }
 }
