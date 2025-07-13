1a8b8492568c42f2799696b2774e85a47974e60f
435THz <delbovemarie@gmail.com>
2025-07-13 13:02:37 +0200

    Remove editors for RogueEssence classes

--- PMDC/Dev/Editors/MobSpawnExtraEditor.cs
+++ PMDC/Dev/Editors/MobSpawnExtraEditor.cs
@@ -1,24 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
-using RogueEssence.Content;
 using RogueEssence.Dungeon;
-using RogueEssence.Data;
-using System.Drawing;
 using RogueElements;
-using Avalonia.Controls;
-using RogueEssence.Dev.Views;
-using System.Collections;
-using Avalonia;
-using System.Reactive.Subjects;
 using PMDC.LevelGen;
-using PMDC.Dungeon;
-using RogueEssence.LevelGen;
-using System.Runtime.Remoting;
-using Avalonia.X11;
-using System.Linq;
-using NLua;
-using RogueEssence.Script;
 
 namespace RogueEssence.Dev
 {
@@ -229,38 +213,6 @@ namespace RogueEssence.Dev
         public override string GetTypeString()
         {
             return "Descriminator";
-        }
-    }
-    public class MobSpawnStatusEditor : Editor<MobSpawnStatus>
-    {
-        public override string GetString(MobSpawnStatus obj, Type type, object[] attributes)
-        {
-            if (obj.Statuses.Count != 1)
-                return string.Format("Status: [{0}]", obj.Statuses.Count.ToString());
-            else
-            {
-                EntrySummary summary = DataManager.Instance.DataIndices[DataManager.DataType.Status].Get(obj.Statuses.GetSpawn(0).ID);
-                return string.Format("Status: {0}", summary.Name.ToLocal());
-            }
-        }
-        public override string GetTypeString()
-        {
-            return "Status";
-        }
-    }
-    public class MobSpawnScriptEditor : Editor<MobSpawnScript>
-    {
-        public override string GetString(MobSpawnScript obj, Type type, object[] attributes)
-        {
-            LuaTable tbl = LuaEngine.Instance.RunString("return " + obj.ArgTable).First() as LuaTable;
-            if (tbl is not null && tbl.Keys.Count > 0)
-                return string.Format("Script: {0} [{1}]", obj.Script.ToString(), tbl.Keys.Count.ToString());
-            else
-                return string.Format("Script: {0}", obj.Script.ToString());
-        }
-        public override string GetTypeString()
-        {
-            return "Spawn Script";
         }
     }
 	public class Intrinsic3ChanceEditor : Editor<Intrinsic3Chance>
